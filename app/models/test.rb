7006f4e2afac4206862147c38582080b071ff884
fkyeaseking <kozyrevkirill7@gmail.com>
2021-11-14 17:03:20 +0300

    created associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: "User"
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+
   def self.find_by_category(category)
     Test.joins("INNER JOIN categories ON categories.id = tests.category_id")
         .where(categories: { title: category })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :created_tests, class_name: "Test", optional: true
+  has_many :tests, through: :results
+
   def find_tests_by_level(level)
     Test.joins("INNER JOIN results ON results.test_id = tests.id")
         .where(results: { user_id: id })
