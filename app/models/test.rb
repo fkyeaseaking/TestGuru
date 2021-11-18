0111fee26f7778022b3cb3788d6b01f3fa8838d3
fkyeaseking <kozyrevkirill7@gmail.com>
2021-11-18 17:20:48 +0300

    added dependent to associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,9 +2,9 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
 
-  has_many :questions
-  has_many :results
-  has_many :users, through: :results
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results, dependent: :destroy
 
   def self.find_by_category(category)
     Test.joins(:category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
   has_many :results
-  has_many :created_tests, class_name: "Test"
-  has_many :tests, through: :results
+  has_many :created_tests, class_name: "Test", dependent: :destroy
+  has_many :tests, through: :results, dependent: :destroy
 
   def find_tests_by_level(level)
-    Test.joins(:results)
-        .where(results: { user_id: id })
-        .where(level: level)
+    tests.where(level: level)
   end
 end
