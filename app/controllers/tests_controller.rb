50cbc88dd3e2725cb2d574c6e15f97f63a7b4ef1
fkyeaseking <kozyrevkirill7@gmail.com>
2022-01-21 22:40:11 +0300

    app logic

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,6 @@
 class TestsController < ApplicationController
-  before_action :find_test, olny: %i[show update destroy]
+  before_action :find_test, only: %i[show update destroy start]
+  before_action :find_user, only: :start
 
   def index
     @tests = Test.all
@@ -39,6 +40,11 @@ class TestsController < ApplicationController
     redirect_to tests_path
   end
 
+  def start
+    @user.tests.push(@test)
+    redirect_to @user.test_passage(@test)
+  end
+
   private
 
   def test_params
@@ -47,5 +53,9 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def find_user
+    @user = User.first
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
-  has_many :results
   has_many :created_tests, class_name: "Test", dependent: :destroy
-  has_many :tests, through: :results, dependent: :destroy
+  has_many :test_passages
+  has_many :tests, through: :test_passages, dependent: :destroy
 
   validates :email, presence: true
 
   def find_tests_by_level(level)
     tests.find_by_level(level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
